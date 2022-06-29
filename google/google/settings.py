417ce1aaaea0adcdbffd0cc449d02eb2e7b60016
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2022-06-29 11:08:47 +0530

    fix error

--- google/google/settings.py
+++ google/google/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -73,8 +73,9 @@ ROOT_URLCONF = 'google.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -155,8 +156,8 @@ AUTHENTICATION_BACKENDS =[
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'logout'
-LOGIN_REDIRECT_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGOUT_REDIRECT_URL = 'login'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '##################'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRETY = '##########'
